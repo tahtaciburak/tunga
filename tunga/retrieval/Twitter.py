d98f2b86df07e70c1b1f59e512dbee5cb5178848
beyza <beyzacanbay34@gmail.com>
2020-07-03 15:32:31 +0300

    add twitter retrieval

--- tunga/retrieval/Twitter.py
+++ tunga/retrieval/Twitter.py
@@ -1,31 +1,33 @@
-def read_tweets_from_user(username, max_tweet_count=50):
-    """
-    Bu method kullanici adi verilen bir twitter kullanicisinin max_tweet_count tane tweetini getirir
-    :param username:
-    :param max_tweet_count:
-    :return: tweets
-    """
-    tweets = []
-    return tweets
+import tweepy as tw
+
+auth = tw.OAuthHandler('imW937M6khWYK3utt6rgUNwC9', 'NHrbDvcnvoqOIJjWKhUXZPAfsToM3k9X8mQoslGF0w24xiJknR')
+auth.set_access_token('246202949-DZ5kRviZM7cBJFyp1gMZw3GytoIsbGMBhNt3pGOW',
+                      'n7G3SblCHd9URjIAugRRBZKqYLUVyDYtf39vuYvgaqE9q')
+api = tw.API(auth)
 
 
-def read_tweets_from_hashtag(hashtag, max_tweet_count=50):
-    """
-    Bu method bir hashtagte atilmis olan son max_tweet_count kadar tweeti getirir.
-    :param hashtag:
-    :param max_tweet_count:
-    :return: tweets
-    """
-    tweets = []
-    return tweets
+def read_tweets_from_user(username, max_tweet_count):
+    user = api.user_timeline(username, count=max_tweet_count)
+
+    user_tweet = []
+    for tweet in user:
+        user_tweet.append(tweet.text)
+    return user_tweet
 
 
-def read_tweets_from_mention(username, max_tweet_count=50):
-    """
-    Bu method bir kullaniciya atilmis olan mentionlari son max_tweet_count kadar tweeti getirir.
-    :param hashtag:
-    :param max_tweet_count:
-    :return: tweets
-    """
-    mentions = []
-    return mentions
+def read_tweets_from_hashtag(hashtag, date, max_tweet_count):
+    search_words = hashtag + "-filter:retweets"
+    date_since = date
+
+    tweets = tw.Cursor(api.search,
+                       q=search_words,
+                       lang="tr",
+                       since=date_since).items(max_tweet_count)
+    date_tweet = []
+    for tweet in tweets:
+        date_tweet.append(tweet.text)
+    return date_tweet
+
+
+def read_tweets_from_mention(hashtag, date, max_tweet_count):
+    return True
