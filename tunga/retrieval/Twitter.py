972edfb001fe07e229a5b9a276319af20d754691
tahtaciburak <tahtaciburak@gmail.com>
2020-07-03 10:47:09 +0300

    Add twitter retrieval base

---
+++ tunga/retrieval/Twitter.py
@@ -0,0 +1,31 @@
+def read_tweets_from_user(username, max_tweet_count=50):
+    """
+    Bu method kullanici adi verilen bir twitter kullanicisinin max_tweet_count tane tweetini getirir
+    :param username:
+    :param max_tweet_count:
+    :return: tweets
+    """
+    tweets = []
+    return tweets
+
+
+def read_tweets_from_hashtag(hashtag, max_tweet_count=50):
+    """
+    Bu method bir hashtagte atilmis olan son max_tweet_count kadar tweeti getirir.
+    :param hashtag:
+    :param max_tweet_count:
+    :return: tweets
+    """
+    tweets = []
+    return tweets
+
+
+def read_tweets_from_mention(username, max_tweet_count=50):
+    """
+    Bu method bir kullaniciya atilmis olan mentionlari son max_tweet_count kadar tweeti getirir.
+    :param hashtag:
+    :param max_tweet_count:
+    :return: tweets
+    """
+    mentions = []
+    return mentions
