da55d37ac5bcdef46d644150c772b5285d18956d
tahtaciburak <tahtaciburak@gmail.com>
2020-07-05 15:24:31 +0300

    Fix footer

--- frontend/src/containers/TheFooter.js
+++ frontend/src/containers/TheFooter.js
@@ -5,12 +5,11 @@ const TheFooter = () => {
   return (
     <CFooter fixed={false}>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">CoreUI</a>
-        <span className="ml-1">&copy; 2020 creativeLabs.</span>
+        <a href="https://github.com/tahtaciburak/tunga" target="_blank" rel="noopener noreferrer">TUNGA</a>
+        <span className="ml-1">&copy; 2020 Made with LOVE </span>
       </div>
       <div className="mfs-auto">
-        <span className="mr-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">CoreUI for React</a>
+        <a href="https://github.com/tahtaciburak/tunga" target="_blank" rel="noopener noreferrer">Version X.X.X</a>
       </div>
     </CFooter>
   )
