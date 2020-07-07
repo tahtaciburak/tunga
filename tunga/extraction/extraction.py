9e7fdf7572445ab208933bb7b08e245652eb0bfe
tahtaciburak <tahtaciburak@gmail.com>
2020-07-07 16:24:32 +0300

    Add extraction module to library

---
+++ tunga/extraction/extraction.py
@@ -0,0 +1,22 @@
+def extract_emojis(text):
+    return ""
+
+
+def extract_mails(text):
+    return ""
+
+
+def extract_urls(text):
+    return ""
+
+
+def extract_hashtags(text):
+    return ""
+
+
+def extract_prices(text):
+    return ""
+
+
+def extract_language(text):
+    return ""
