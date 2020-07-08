d6c0e2f40c3ea6e7acc4d53596464b316c9943e7
beyza <beyzacanbay34@gmail.com>
2020-07-08 16:14:58 +0300

    fixed error for twitter api

--- tunga/retrieval/Twitter.py
+++ tunga/retrieval/Twitter.py
@@ -1,5 +1,5 @@
 import tweepy as tw
-import config
+from tunga.retrieval import config
 auth = tw.OAuthHandler(config.api_key, config.api_secret,)
 auth.set_access_token(config.access_token,config.token_secret)
 api = tw.API(auth)
