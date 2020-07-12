2a4c8bcc2129b8073f5b90882df1a1b5b1888a4b
beyza <beyzacanbay34@gmail.com>
2020-07-12 22:33:35 +0300

    create topic_extract function

--- tunga/machine_learning/topic_modelling/topic_extraction.py
+++ tunga/machine_learning/topic_modelling/topic_extraction.py
@@ -2,23 +2,27 @@ import pandas as pd
 from sklearn.decomposition import LatentDirichletAllocation as LDA
 from sklearn.feature_extraction.text import CountVectorizer
 
-data = pd.read_csv("../../../datasets/haberler.csv")
-count_vectorizer = CountVectorizer()
-count_data = count_vectorizer.fit_transform(data['text'])
+
+def topic_extract(data_path,data_column,topic_number,number_word):
+
+    data = pd.read_csv(data_path)
+    count_vectorizer = CountVectorizer()
+    count_data = count_vectorizer.fit_transform(data[data_column])
 
 
-def print_topics(model, count_vectorizer, n_top_words):
-    words = count_vectorizer.get_feature_names()
-    for topic_idx, topic in enumerate(model.components_):
-        print("Topic_Num: {} ".format(topic_idx))
-        print(" ".join([words[i] for i in topic.argsort()[:-n_top_words - 1:-1]]))
+    def print_topics(model, count_vectorizer, n_top_words):
+        words = count_vectorizer.get_feature_names()
+        for topic_idx, topic in enumerate(model.components_):
+            print("Topic_Num: {} ".format(topic_idx))
+            print(" ".join([words[i] for i in topic.argsort()[:-n_top_words - 1:-1]]))
 
 
-number_topics = 7
-number_words = 50
+    number_topics = topic_number
+    number_words = number_word
 
-lda = LDA(n_components=number_topics, n_jobs=16)
-lda.fit(count_data)
+    lda = LDA(n_components=number_topics, n_jobs=16)
+    lda.fit(count_data)
 
-print("Topics found via LDA:")
-print_topics(lda, count_vectorizer, number_words)
+    return("Topics found via LDA: ",print_topics(lda, count_vectorizer, number_words))
+
+print(topic_extract("../../../datasets/haberler.csv","text",7,15))
