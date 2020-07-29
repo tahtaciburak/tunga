a4c3d0e4ad6cc5e178a9e06606a68535ccac81e8
tahtaciburak <tahtaciburak@gmail.com>
2020-07-29 13:53:32 +0300

    Add torch

---
+++ tests/test_MachineLearning.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+class TestMachineLearning(unittest.TestCase):
+    def test_sentiment(self):
+        from tunga.machine_learning.sentiment_analysis import bert_sentiment
+        res = bert_sentiment("çok iyi bir yorum yapacağım")
+        self.assertIsNotNone(res)
+
+    def test_keyword_extraction(self):
+        from tunga.machine_learning.keyword_extraction import rake
+        res = rake("bu bir test cümlesidir.")
+        self.assertIsNotNone(res)
+
+    def test_topic_modelling(self):
+        from tunga.machine_learning.topic_modelling import topic_modeller
+        res = topic_modeller(["test", "topic", "mopic"])
+        self.assertIsNotNone(res)
+
+
+if __name__ == '__main__':
+    unittest.main()
